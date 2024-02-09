4bdc8023384bc624e927c75a8fe2607999006100
IsmaelPerezChacon <159494487+IsmaelPerezChacon@users.noreply.github.com>
2024-02-09 16:55:23 -0600

    Add files via upload

---
+++ EVA1_2_IDENTIFICADORES.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_identificadores;
+
+/**
+ *
+ * @author Mimas
+ */
+public class EVA1_2_IDENTIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //TODO code application logic here
+        //VARIABLES:
+        //short int byte long --> enteros
+        //float double --> numeros con decimal
+        //char --> character - carateres
+        //boolean --> verdadero o falso
+        //String --> cadenas de texto
+        //si no se declara una variable, no se puede usar
+        int edad;   //declaración de una variable
+        String nombre;
+        double salario;
+        
+        //ASIGNACION DE VALORES
+        //IDENTIFICADOR = VALOR;
+        //se coloca el valor 50 dentro de la variable edad
+        edad = 22;
+        nombre = "ISMAEL PEREZ";
+        salario = 123.56;
+        //COMO LOS USAMOS:
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+        
+    }
+    
+}
