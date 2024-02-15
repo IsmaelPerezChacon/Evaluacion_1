e8a2f2aff9e2e3e3b0c6bc82b7e5e32e8f4f820f
IsmaelPerezChacon <159494487+IsmaelPerezChacon@users.noreply.github.com>
2024-02-15 17:10:49 -0600

    Add files via upload

---
+++ EVA1_4_.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_;
+
+/**
+ *
+ * @author Mimas
+ */
+public class EVA1_4_ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int edad; // DECLARACION
+        String nombre;
+        String carrera;
+        nombre = "ismael perez";
+        carrera = "Sistemas Computacionales";
+        edad = 21; //ASIGNACION NO "IGUALDAD" -- EL SIMBOLO "=" SIGNIFICA ASIGNAR ALGO.
+        System.out.println("Tu edad es:");
+        System.out.println(edad);
+        System.out.println("Tu Nombre es:");
+        System.out.println(nombre);
+        System.out.println("Tu Carrera es:");
+        System.out.println(carrera);
+        
+        
+        
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+}
